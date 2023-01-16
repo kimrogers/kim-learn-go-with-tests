b72957a60d6461783456c6e491aa2ef15372be59
kimrogers <kim.rogers@cultureamp.com>
2023-01-16 14:57:40 +1100

    change of location, more languages

--- hello-world/v9/hello_test.go
+++ hello-world/v9/hello_test.go
@@ -2,28 +2,11 @@ package main
 
 import "testing"
 
-// func TestHello(t *testing.T) {
-// 	t.Run("saying hello to people", func(t *testing.T) {
-// 		got := Hello("Chris")
-// 		want := "Hello, Chris"
-
-// 		if got != want {
-// 			t.Errorf("got %q want %q", got, want)
-// 		}
-// 	})
-// 	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
-// 		got := Hello("")
-// 		want := "Hello, World"
-
-// 		if got != want {
-// 			t.Errorf("got %q want %q", got, want)
-// 		}
-// 	})
-// }
-
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris", "")
+		// var got string = ""
+		// got = "hello"
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
@@ -40,11 +23,17 @@ func TestHello(t *testing.T) {
 		assertCorrectMessage(t, got, want)
 	})
 
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("", "French")
+		want := "Bonjour, World"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("I got %q but I want %q", got, want)
 	}
 }
