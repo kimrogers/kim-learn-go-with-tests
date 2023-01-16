4ee2c3b1477f9fe765cb47e8dc32771dfbb2ece3
kimrogers <kim.rogers@cultureamp.com>
2023-01-16 12:10:26 +1100

    hello world progress

--- hello-world/v1/hello.go
+++ hello-world/v1/hello.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string, lang string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	if lang == "Spanish" {
+		return "Hola, " + name
+	}
+	return englishHelloPrefix + name
+}
+
 func main() {
-	fmt.Println("Hello, world")
+
+	fmt.Println(Hello("world", ""))
 }
